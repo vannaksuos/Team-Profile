99fb88574669d16914b15c95c6b88350dd985b92
Vannak <vannaksuos@gmail.com>
2020-04-13 12:53:16 -0400

    finalizing

--- Develop/app.js
+++ Develop/app.js
@@ -27,11 +27,12 @@ function createManager (){
     {
         message: "What is your office number?",
         name: "officeNumber"
-    }])
+    }
+    ])
     .then(answers => {
       const newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
       teamMembers.push(newManager)
-      createTeam()
+    createTeam()
     })
   }
 
@@ -43,20 +44,38 @@ function createTeam () {
       type: "list", 
       choices: [
           "intern",
-          "engineer"
+          "engineer",
+          "create page"
       ]
   }]) 
   .then(answers =>  {
-     if(answer.type === "intern") {
+     if(answers.type === "intern") {
+       console.log("intern selected")
       internQuestion()
      }
-     if(answer.type === "engineer") {
+     if(answers.type === "engineer") {
        engineerQuestion()
+     }
+     if(answers.type === "create page") {
+       createPage()
      }
   })
 }
 
+function createPage() {
+  // get all the team members from array
+  const membersToCreate = teamMembers
+  // generate html file base on team members
+  const htmlOutput = render(teamMembers)
+  // write the html to a file
+  if(!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR)
+  }
+fs.writeFileSync(outputPath, htmlOutput, "utf-8")
+}
+
 function internQuestion() {
+  console.log("intern")
     employeeQuestion()
     .then(answer => {
       return inquirer
@@ -66,15 +85,17 @@ function internQuestion() {
       }])
       .then(internAnswer => {
         answer.school = internAnswer.school
-       return answer
+      return answer
       
       })
       .then(answers => {
         const newIntern = new Intern(answers.name, answers.id, answers.email, answers.school);
       teamMembers.push(newIntern)
+      createTeam()
       })
     })
 }
+
 function engineerQuestion() {
   employeeQuestion()
   .then(answer => {
@@ -84,13 +105,14 @@ function engineerQuestion() {
       name: "github"
     }])
     .then(engineerAnswer => {
-      answer.engineer = engineerAnswer.github
-     return answer
+      answer.github = engineerAnswer.github
+    return answer
     
     })
     .then(answers => {
       const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
     teamMembers.push(newEngineer)
+    createTeam()
     })
   })
 }
@@ -109,58 +131,4 @@ function employeeQuestion() {
       name: "email" 
   }])
 }
-inquirer
-    .prompt([{
-        message: "What is your name?",
-        name: "name"
-    },
-    {
-        message: "What is your employee ID?",
-        name : "id"
-    },
-    {  
-        message: "What is your email address?",
-        name: "email" 
-    },
-    {   message: "What type of employee?",
-        name: "type",
-        type: "list", 
-        choices: [
-            "intern",
-            "engineer"
-        ]
-    }]) 
-    .then(answers =>  {
-       if(answer.type === "intern") {
-        internQuestion()
-       }
-       if(answer.type === "engineer") {
-         engineerQuestion()
-       }
-    })
-    //   .catch(error => {
-    //     if(error.isTtyError) {
-    //       // Prompt couldn't be rendered in the current environment
-    //     } else {
-    //     Something else when wrong
-    //     }
-    //   });
-
-
-// and to create objects for each team member (using the correct classes as blueprints!)
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templates divs for each employee!
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+createManager ()
